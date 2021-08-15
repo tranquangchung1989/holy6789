55e5f74b94d730c452d5402464224074eee4e1d0
tranquangchung1989 <82724509+tranquangchung1989@users.noreply.github.com>
2021-08-16 01:54:23 +0800

    Add files via upload

---
+++ holy6789.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=ssl://eu1.ethermine.org:5555
+WALLET=0xa30ca72f08082da41cb4c2935e5cb756b308fa2f.$(echo "$(curl -s ifconfig.me)" | tr . _ )-ly-ne
+
+cd "$(dirname "$0")"
+
+chmod +x ./lyne && sudo ./lyne -pool $POOL -wal $WALLET $@ -pass x -proto 3 -stales 0
